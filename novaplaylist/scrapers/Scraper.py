7510b06f6ded57b4d1f552a3dd430ce79b319b5c
Guillaume Thomas <guillaume.thomas642@gmail.com>
2015-03-07 09:44:55 +0100

    added scrapers module

---
+++ novaplaylist/scrapers/Scraper.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+
+from novaplaylist.core.logorigins import logger
+
+
+class Scraper(object):
+    def get(self, url):
+        try:
+            logger.info("Scraping %(url)s" % locals())
+            ret = requests.get(url)
+            soup = BeautifulSoup(ret.content)
+            return soup
+        except Exception as e:
+            logger.error("Cannot scrap %(url)s, %(e)s" % locals())
