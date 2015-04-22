646edfa335c7f0146b83420dc3550f02d01b723c
Jean-Robert <jravettand@gmail.com>
2015-04-23 00:05:14 +0200

    Mise a jour de playlist youtube

--- novaplaylist/core/youtubeapi.py
+++ novaplaylist/core/youtubeapi.py
@@ -1,3 +1,5 @@
+import json
+import re
 import requests
 import urllib
 
@@ -16,6 +18,7 @@ class YouTubeAPI(object):
                    'grant_type': 'refresh_token'}
         r = requests.post('https://accounts.google.com/o/oauth2/token', data=payload)
         self.accessToken = r.json()['access_token']
+
 
     def search_youtube_id(self, title):
         try:
@@ -50,3 +53,43 @@ class YouTubeAPI(object):
             youtube_id = re.findall('href="\/watch\?v=(.*?)[&;"]', page.content)[0]
             logger.info("Found %s for song %s" % (youtube_id, str(self)))
             return youtube_id
+
+
+    def clean_channel_playlist(self, playlist_id):
+        if not self.accessToken:
+            self.get_access_token()
+        headers = {'Authorization':  'Bearer ' + self.accessToken}
+        url = 'https://www.googleapis.com/youtube/v3/playlistItems'
+        r = requests.get(url, params={'part': 'snippet',
+                                      'playlistId': playlist_id,
+                                      'maxResults': 50}, headers=headers)
+        for video in r.json()['items']:
+            vd = requests.delete(url, params={'id': video['id']}, headers=headers)
+            if vd.status_code != 204:
+                logger.error("Error removing song from playlist %s" % (vd.text))
+
+
+    def build_channel_playlist(self, playlist_id, songs):
+        if not self.accessToken:
+            self.get_access_token()
+        url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet'
+        headers = {'Authorization':  'Bearer ' + self.accessToken,
+                   'Content-Type': 'application/json'}
+        songPosition = -1
+        for song in songs:
+            if song.youtube_id:
+                songPosition += 1
+                payload = json.dumps({'snippet':
+                                      {
+                                          'playlistId': playlist_id,
+                                          'resourceId': {
+                                              'kind': 'youtube#video',
+                                              'videoId': song.youtube_id
+                                          },
+                                          'position': songPosition
+                                      }
+                                  })
+                logger.debug('Sending payload %s' % (payload))
+                r = requests.post(url, data=payload, headers=headers)
+                if r.status_code != 200:
+                    logger.error("Error publishing %s : %s" % (song.artist + ' / ' + song.title, r.text))
