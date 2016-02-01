8914c457ffa5320a8a12f035ec42f2ff051389c9
Jean-Robert <jravettand@gmail.com>
2016-02-01 21:45:31 +0100

    Update FipScraper for new ajax syntax

--- novaplaylist/scrapers/FipScraper.py
+++ novaplaylist/scrapers/FipScraper.py
@@ -13,19 +13,20 @@ logger = logging.getLogger('nova-playlist')
 
 
 class FipScraper(Scraper):
-    def parse(self, url):
-        soup = BeautifulSoup(requests.get(url).json()[1]['data'])
+    def parse(self, url, data):
+        soup = BeautifulSoup(requests.post(url, data).json()[1]['data'])
         songs = []
         if not soup:
             return songs
         divs = soup.select("div.son")
         for div in divs:
-            titles = div.select("p.titre_title")
-            artists = div.select("p.titre_artiste")
+            subdiv = div.select("div.list-song-right")[0].select("div.texte")[0]
+            titles = subdiv.select("p.titre_title")
+            artists = subdiv.select("p.titre_artiste")
             if titles and artists and titles[0].string != 'FIP ACTUALITE':
                 songs.append(Song(
-                    title=titles[0].string.title(),
-                    artist=artists[0].string.title()
+                    title=titles[0].string.title().strip(),
+                    artist=artists[0].get_text()[5:].strip()
                 ))
         return songs
 
@@ -33,7 +34,16 @@ class FipScraper(Scraper):
         ts = ts_beg
         songs = []
         while ts < ts_end:
-            url = 'http://www.fipradio.fr/fip_titres_diffuses/ajax/search/%s' % int(time.mktime(ts.timetuple()))
-            songs.extend(self.parse(url))
+            url = 'http://www.fipradio.fr/system/ajax/'
+            data = {'display_search': 1,
+                    'select_radio': 7,
+                    'select_jour': int(time.mktime(datetime.datetime(ts.year, ts.month, ts.day).timetuple()) - \
+                                       time.mktime(datetime.datetime(1970, 1, 1).timetuple()) - \
+                                       3600),
+                    'select_heure': ts.hour,
+                    'select_minute': ts.minute,
+                    'form_build_id': 'form-md8wUEVgOEDkqf-6e7IqXdpoYrgaS2bYPCFTtbIQ22k',
+                    'form_id': 'fip_titres_diffuses_cruiser_form_search_date'}
+            songs.extend(self.parse(url, data))
             ts += datetime.timedelta(hours=1)
         return songs
